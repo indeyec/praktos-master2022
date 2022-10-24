98340fb94acb0871c41ca443111d8c9b9413de23
Vladislav <vlad.shilov03@mail.ru>
2022-10-24 22:47:57 +0700

    готово

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
 
-#admin.site.register(Book)
+
 admin.site.register(Author)
 admin.site.register(Genre)
-#admin.site.register(BookInstance)
+
 
 class BooksInline(admin.TabularInline):
     model = Book
